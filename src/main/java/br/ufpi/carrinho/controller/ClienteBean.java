e91a4b26086b442e62c5248d0efbdcc0f6cce017
Fabricio <fabricio.a.s@hotmail.com>
2018-03-15 19:16:56 -0300

    Refatoração feita no clienteBean, adicionado o clienteDAO

---
+++ src/main/java/br/ufpi/carrinho/controller/ClienteBean.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package br.ufpi.carrinho.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.ufpi.carrinho.dao.ClienteDao;
+import br.ufpi.carrinho.model.Cliente;
+import br.ufpi.carrinho.util.Utils;
+
+/**
+ * @author Renato
+ *
+ */
+@Named
+@ViewScoped
+public class ClienteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private ClienteDao clienteDao;
+
+	private Cliente cliente;
+
+	public ClienteBean() {
+	}
+
+	@PostConstruct
+	public void init() {
+		this.cliente = new Cliente();
+	}
+	
+	public List<Cliente> lista(){
+		return clienteDao.listar();
+	}
+
+	public void salvar() {
+		String cpf = Utils.cpfSoComNumero(cliente.getCpf());
+		
+		if (clienteDao.buscar(cpf) == null) {
+			cliente.setCpf(cpf);
+			clienteDao.salvar(cliente);
+			cliente = new Cliente();
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Sucesso!", "Cliente salvo com sucesso!"));
+		}else {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Erro!", "Já existe um cliente com o cpf " + cpf));
+		}
+
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
---
+++ src/main/java/br/ufpi/carrinho/dao/ClienteDao.java
@@ -0,0 +1,67 @@
+package br.ufpi.carrinho.dao;
+
+import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import br.ufpi.carrinho.model.Cliente;
+import br.ufpi.carrinho.util.Utils;
+
+@Stateless
+public class ClienteDao implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public ClienteDao() {
+	}
+
+	public Cliente procurarClientePorEmailSenha(String email, String senha) {
+		TypedQuery<Cliente> query = em
+				.createQuery("SELECT c FROM Cliente c WHERE c.email = :email AND c.senha = :senha", Cliente.class);
+		query.setParameter("email", email);
+		query.setParameter("senha", Utils.convertStringToMd5(senha));
+
+		List<Cliente> clientes = query.getResultList();
+
+		if (clientes.size() == 1)
+			return clientes.get(0);
+
+		return null;
+	}
+
+
+	public List<Cliente> listar() {
+		TypedQuery<Cliente> query = em.createQuery("Select c from Cliente c", Cliente.class);
+		return query.getResultList();
+	}
+
+	public void salvar(Cliente cliente) {
+		String senha = Utils.convertStringToMd5(cliente.getSenha());
+		cliente.setSenha(senha);
+		em.merge(cliente);
+	}
+
+	public Cliente buscar(String cpf) {
+		Cliente c = null;
+		try {
+			TypedQuery<Cliente> query = em.createQuery("Select c from Cliente c WHERE c.cpf like :cpf", Cliente.class)
+					.setParameter("cpf", cpf);
+			c = query.getSingleResult();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return c;
+	}
+
+}
